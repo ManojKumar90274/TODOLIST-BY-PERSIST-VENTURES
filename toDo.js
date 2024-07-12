5da5b8e232939f103b1a65471b9e003e3dff385e
Manoj Kumar <122885692+ManojKumar90274@users.noreply.github.com>
2024-07-12 15:25:31 +0530

    Add files via upload

---
+++ toDo.js
@@ -0,0 +1,19 @@
+let input=document.getElementById("inp");
+let text=document.querySelector(".text");
+
+
+function add(){
+    if(input.value == " "){
+        alert("please enter the task")
+    } else {
+        let newEle=document.createElement("ul");
+        newEle.innerHTML= `${input.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        input.value=" ";
+        newEle.querySelector("i").addEventListener("click",remove);
+        function remove(){
+            newEle.remove();
+        }
+        
+    }
+}
